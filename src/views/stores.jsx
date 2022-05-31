02954a813f4b4be5ec0c7698500bf51a4f19e1f6
dinaAlsaid <dinamalsaid@gmail.com>
2022-05-31 17:01:30 +0300

    store card

--- src/views/stores.jsx
+++ src/views/stores.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { RegisterContext } from "../context/registration";
+import { Card, Button } from "react-bootstrap";
 
 export default function Stores() {
   const registration = useContext(RegisterContext);
@@ -19,12 +20,21 @@ export default function Stores() {
       setStore([...response.data.data]);
     };
     getStores();
-  }, []);//eslint-disable-line
+  }, []); //eslint-disable-line
 
   return (
     <>
       {stores.map((store) => {
-        return <div>{store.name}</div>;
+        return (
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="https://scotturb.com/wp-content/uploads/2016/11/product-placeholder.jpg" />
+            <Card.Body>
+              <Card.Title>{store.name}</Card.Title>
+              <Card.Text>{store.description}</Card.Text>
+              <Button variant="primary">Shop here</Button>
+            </Card.Body>
+          </Card>
+        );
       })}
     </>
   );
